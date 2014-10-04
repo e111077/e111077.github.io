321b77dc1ad223f591f1f2a9b1fb558869761b84
Elliott Marquez <marquez@mit.edu>
2014-10-04 14:34:46 -0400

    Fixed put

--- App/code.js
+++ App/code.js
@@ -9,18 +9,19 @@ function podURL() {
 }
 
 
-function reload() {
+function reload () {
 
 	var request = new XMLHttpRequest();
 
 	// just fetch everything, for now, since queries don't work yet
-	request.open("GET", podURL()+"/_nearby", true);
+	request.open("GET", podURL()+"/_active", true);
 	if (etag !== null) {
 		request.setRequestHeader("Wait-For-None-Match", etag);
 	}
 
 	request.onreadystatechange = function() {
 		if (request.readyState==4 && request.status==200) {
+			console.log("GOT");
     		handleResponse(request.responseText);
     	}
  	}
@@ -30,56 +31,77 @@ function reload() {
 
 function handleResponse(responseText) {
 	var responseJSON = JSON.parse(responseText);
-	etag = responseJSON._etag;
 	var all = responseJSON._members;
 	var messages = [];
 	for (var i=0; i<all.length; i++) {
 		var item = all[i];
 		var completed = item.completed;
 		// consider the 'text' property to be the essential one
-		if ('task' in item && 'completed' in item) {
-			messages.push([item, completed])
+		if ('appName' in item && item.appName == "CrossTask") {
+			messages.push(item)
 		}
 	}
-	messages.sort(function(a){return a._owner});
+	messages.sort(function(a, b){
+    if(a._owner < b._owner) return -1;
+    if(a._owner > b._owner) return 1;
+    return 0;
+	});
 	
 	// not being clever, just remove and re-create the whole "out" element
 	var out = document.getElementById("out")
 	while(out.firstChild) { out.removeChild(out.firstChild) }
 	for (i=0; i<messages.length; i++) {
-		var message = messages[i][0];
-		var completed = messages[1];
+		var message = messages[i];
+		console.log(message);
+		var completed = message.completed;
+		var splitMessage = message._id.split("r");
+		var etag = splitMessage[splitMessage.length-1];
 		message.timeDate = new Date(Number(message.time))
 		var tr = document.createElement("tr");
 		if (completed) {
-			tr.innerHTML = "<td> "+message._owner+" </td><td>"+ message.task +"</td><td><input type='checkbox' id='"+ etag +"'></td>";
+			tr.innerHTML = "<td> "+message._owner+" </td><td>"+ message.task +"</td><td><input type='checkbox' id='"+ etag +"' checked='checked'></td>";
 		} else {
-			tr.innerHTML = "<td> "+message._owner+" </td><td>"+ message.task +"</td><td><input type='checkbox' id='"+ etag +"' checked></td>";
+			tr.innerHTML = "<td> "+message._owner+" </td><td>"+ message.task +"</td><td><input type='checkbox' id='"+ etag +"'></td>";
 		}
 		out.appendChild(tr);
 	}
 
-	$('[type="checkbox"]').click(function(){
-		var etag = this.id;
+	$('[type="checkbox"]').mouseup(function(){
+		function putData (JSONData, etag) {
+			var request = new XMLHttpRequest();
+			request.open("PUT", podURL()+'/r'+etag);
+	    	request.onreadystatechange = function() {
+	            if (request.readyState==4 && request.status==201) {
+					// why does this always print null, even though it's not?
+					// console.log("Location:", request.getResponseHeader("Location"));
+	     		}
+			}
+			request.setRequestHeader("Content-type", "application/json");
+			var content = JSON.stringify(JSONData);
+			request.send(content);
+		};
+
 		var request = new XMLHttpRequest();
-		request.open("PUT", podURL()+'/r'+etag);
-    	request.onreadystatechange = function() {
-            if (request.readyState==4 && request.status==201) {
-				// why does this always print null, even though it's not?
-				// console.log("Location:", request.getResponseHeader("Location"));
-     		}
-		}
-		request.setRequestHeader("Content-type", "application/json");
-		var content = JSON.stringify({completed:this.checked});
-		request.send(content);
+		var etag = this.id;
+		var thisBox = $(this);
+		request.open("GET", podURL()+"/r" + etag, true);
+		request.onreadystatechange = function() {
+			if (request.readyState==4 && request.status==200) {
+	    		var responseJSON = JSON.parse(request.responseText);
+	    		responseJSON.completed = thisBox.is(":checked");
+	    		putData(responseJSON, etag);
+	    	}
+	 	}
+
+		request.send();
+		reload();
 	});
 	document.getElementById("chat").style.visibility = "visible"
 	
 	// wait for 100ms then reload when there's new data.  If data
 	// comes faster than that, we don't really want it.
-	setTimeout(reload, 50);
+	setTimeout(reload, 1000);
 }
-
 
 function newmsg() {
     var message = document.getElementById("message").value;
@@ -94,7 +116,8 @@ function newmsg() {
      		}
 		}
 		request.setRequestHeader("Content-type", "application/json");
-		var content = JSON.stringify({task:message, completed:false});
+		message.completed = false;
+		var content = JSON.stringify({task:message, appName:"CrossTask"});
 		request.send(content);
 	} 
 }

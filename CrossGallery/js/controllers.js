d12373b07f39b11496e0eaa36f65441df1073cf3
Elliott Marquez <marquez@mit.edu>
2014-12-04 20:39:38 -0500

    fixed an issue with identification

--- CrossGallery/js/controllers.js
+++ CrossGallery/js/controllers.js
@@ -2,10 +2,22 @@ angular.module('CrossGallery.services', []).controller('mediaController', functi
     $scope.allMedia = [];
     crossCloudAPIService.requestAllMediaData().then(
         function(media) {
+            for (var i = 0; i < media.length; i++) {
+              var id = media[i]._id;
+              id = id.split('/').join('').split('.').join('').split(':').join('');
+              media[i].id = id;
+            }
+
             $scope.allMedia = media;
              
         }, function(error) {},
         function(media) {
+            for (var i = 0; i < media.length; i++) {
+              var id = media[i]._id;
+              id = id.split('/').join('').split('.').join('').split(':').join('');
+              media[i].id = id;
+            }
+
             $scope.allMedia = media;
     });
 });
